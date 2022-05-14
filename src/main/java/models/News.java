2a23c346a21df8d5183ed5ac3ea7a7bb173d6dbf
gilbertKorir <jerikorir@gmail.com>
2022-05-14 13:46:57 +0300

    ft: generated a class to hold general news and department news

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,19 @@
 package models;
 
 public class News {
+    private int id;
+    private int user_id;
+    private int department_id;
+    private String title;
+    private String news_type;
+    private String description;
+    private final String TYPE_OF_NEWS = "general";
+
+    public News(String title, String description, int user_id) {
+        this.user_id = user_id;
+        this.department_id = 0;
+        this.title = title;
+        this.news_type = TYPE_OF_NEWS;
+        this.description = description;
+    }
 }
