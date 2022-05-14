99c72f0f620ea62ed4cc9edb3afa3041f8ae6c69
gilbertKorir <jerikorir@gmail.com>
2022-05-14 14:31:08 +0300

    ft: generated getters and setters for the class departments

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,14 +1,38 @@
 package models;
 
 public class Departments {
+    private int id;
     private String name;
     private String description;
-    private int id;
     private int size;
 
     public Departments(String name, String description, int size) {
         this.name = name;
         this.description = description;
         this.size = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 }
