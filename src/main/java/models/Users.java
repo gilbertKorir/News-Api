03db9024261cec1cf390b96b4095dbde6ba1f82a
gilbertKorir <jerikorir@gmail.com>
2022-05-14 14:24:45 +0300

    ft: generated equals and hascode overriding methods

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Users {
     private int id;
     private String name;
@@ -27,4 +29,26 @@ public class Users {
     public void setId(int id) {
         this.id = id;
     }
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return id == users.id &&
+                Objects.equals(name, users.name) &&
+                Objects.equals(position, users.position) &&
+                Objects.equals(staff_role, users.staff_role) ;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, position, staff_role);
+    }
 }
+
+
+
+
+
+
+
+
