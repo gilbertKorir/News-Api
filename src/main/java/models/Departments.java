37c3b787b1c5ac055b3a855260fd6a3beae4c4af
gilbertKorir <jerikorir@gmail.com>
2022-05-14 14:28:10 +0300

    ft: created a class Deparments with parameters and constructor

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,14 @@
 package models;
 
 public class Departments {
+    private String name;
+    private String description;
+    private int id;
+    private int size;
+
+    public Departments(String name, String description, int size) {
+        this.name = name;
+        this.description = description;
+        this.size = 0;
+    }
 }
