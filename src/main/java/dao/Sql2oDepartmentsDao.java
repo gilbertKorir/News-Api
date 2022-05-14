06f117a46e3e6239f183e8c5b2560194eb48f534
gilbertKorir <jerikorir@gmail.com>
2022-05-14 17:03:42 +0300

    ft:generated sql2odepartments to run the department database

--- src/main/java/dao/Sql2oDepartmentsDao.java
+++ src/main/java/dao/Sql2oDepartmentsDao.java
@@ -1,4 +1,44 @@
 package dao;
 
-public class Sql2oDepartmentsDao {
+import models.Departments;
+import models.News;
+import models.Users;
+
+import java.util.List;
+
+public class Sql2oDepartmentsDao implements DepartmentsDao {
+    @Override
+    public void add(Departments department) {
+
+    }
+
+    @Override
+    public void addUserToDepartment(Users user, Departments department) {
+
+    }
+
+    @Override
+    public List<Departments> getAll() {
+        return null;
+    }
+
+    @Override
+    public Departments findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Users> getAllUsersInDepartment(int department_id) {
+        return null;
+    }
+
+    @Override
+    public List<News> getDepartmentNews(int id) {
+        return null;
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
