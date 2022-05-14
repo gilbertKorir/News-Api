5e48c4ef415e1ca1fa0ceec5bca22c76639b15a8
gilbertKorir <jerikorir@gmail.com>
2022-05-14 13:52:19 +0300

    ft: generated getters and setters for the class news

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -16,4 +16,41 @@ public class News {
         this.news_type = TYPE_OF_NEWS;
         this.description = description;
     }
+
+    public News(String title, String description,int department_id, int user_id){
+        this.title = title;
+        this.description = description;
+        this.user_id=user_id;
+        this.department_id = department_id;
+        this.news_type="department";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getNews_type() {
+        return news_type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
