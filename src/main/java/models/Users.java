e4a0db717ee78f1735ebb5ae666ef38ee0966ee8
gilbertKorir <jerikorir@gmail.com>
2022-05-14 14:16:56 +0300

    ft: generated a class Users with parameters and the constructor

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Users {
+    private int id;
+    private String name;
+    private String position;
+    private String staff_role;
+
+    public Users(String name, String position, String staff_role) {
+        this.name = name;
+        this.position = position;
+        this.staff_role = staff_role;
+    }
+
 }
